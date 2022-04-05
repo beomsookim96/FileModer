e219aab05be3c6ded74f2c497f636726221a3344
rlaqjatn7177 <rlaqjatn7177@gmail.com>
2022-04-05 18:19:40 +0900

    Csv to Json File

--- src/main/java/com/example/demo1/HelloApplication.java
+++ src/main/java/com/example/demo1/HelloApplication.java
@@ -13,8 +13,9 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         stage.setMaxWidth(240);
+        stage.setMinWidth(240);
         stage.getIcons().add(new Image("file:src/main/resources/com/example/demo1/icon/logo-dark.png"));
-        Scene scene = new Scene(fxmlLoader.load(), 240, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 240, 800);
         stage.setTitle("JSONModer");
         stage.setScene(scene);
         stage.show();
